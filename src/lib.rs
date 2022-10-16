f6dbd7f08923808d7ac0ed12e08ac81f7564f313
celeri <celestin.bouchet@gmail.com>
2022-10-16 10:36:55 +0200

    added EnumIter to Country

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use std::ops::AddAssign;
 #[cfg(feature = "async")]
 use reqwest::Method;
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumIter;
 use url::Url;
 
 const BASE_URL: &str = "https://newsapi.org/v2";
@@ -82,7 +84,7 @@ impl ToString for Endpoint
     }
 }
 
-#[derive(Serialize, Deserialize, Copy, Clone)]
+#[derive(Serialize, Deserialize, Copy, Clone, EnumIter)]
 pub enum Country
 {
     US,
